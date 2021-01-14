bb3dc1547217b7513c5062f2e2689e19bc237a4b
Tobias Reich <tobias.reich.ich@gmail.com>
2021-01-14 17:24:51 +0100

    Option to change reported audit

--- src/index.js
+++ src/index.js
@@ -20,12 +20,12 @@ const runLighthouse = async (url, flags = {}, config = null) => {
 	return lhr
 }
 
-const createReport = async (endpoint, headers, event, url) => {
+const createReport = async (endpoint, headers, event, url, audit) => {
 	const lhr = await runLighthouse(url, {
 		chromeFlags: [ '--headless' ]
 	})
 
-	const speedIndex = lhr.audits['speed-index']
+	const speedIndex = lhr.audits[audit]
 
 	const action = {
 		key: 'Speed Index',
@@ -39,9 +39,10 @@ const endpoint = process.env.ACKEE_ENDPOINT
 const token = process.env.ACKEE_TOKEN
 const event = { id: process.env.ACKEE_EVENT_ID }
 const url = process.env.URL
+const audit = process.env.AUDIT ?? 'speed-index'
 
 const headers = new Headers({
 	Authorization: `Bearer ${ token }`
 })
 
-createReport(endpoint, headers, event, url)
+createReport(endpoint, headers, event, url, audit)
